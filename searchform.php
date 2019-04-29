83646942616e1081e7f332036a8d93662a4d554a
Chetan Prajapati <hello@chetanprajapati.com>
2019-04-29 10:09:18 +0530

    Update searchform.php
    - Search form layout for Training Team Site.

--- searchform.php
+++ searchform.php
@@ -17,10 +17,10 @@ namespace WordPressdotorg\Forums;
 	<label for="s" class="screen-reader-text"><?php _ex( 'Search for:', 'label', 'wporg-forums' ); ?></label>
 	<?php
 		if ( 'wporg_compat_before_single_view' === current_action() ) {
-			$placeholder = _x( 'Search this forum', 'placeholder', 'wporg-forums' );
+			$placeholder = _x( 'Search Lesson Plans and Workshop Ideas', 'placeholder', 'wporg-forums' );
 			$project     = wporg_support_get_compat_object();
 		} else {
-			$placeholder = _x( 'Search forums', 'placeholder', 'wporg-forums' );
+			$placeholder = _x( 'Search Lesson Plans and Workshop Ideas', 'placeholder', 'wporg-forums' );
 			$project     = null;
 		}
 	?>
@@ -28,7 +28,7 @@ namespace WordPressdotorg\Forums;
 	<?php if ( $project ) : ?>
 	<input type="hidden" name="intext" value="<?php echo esc_attr( $project->prefixed_title ); ?>" />
 	<?php endif; ?>
-	<button class="button button-primary button-search"><i class="dashicons dashicons-search"></i><span class="screen-reader-text"><?php _e( 'Search forums', 'wporg-forums' ); ?></span></button>
+	<button class="button button-primary button-search"><i class="dashicons dashicons-search"></i><span class="screen-reader-text"><?php _e( 'Search Lesson Plans and Workshop Ideas', 'wporg-forums' ); ?></span></button>
 </form>
 
 <?php endif; ?>
