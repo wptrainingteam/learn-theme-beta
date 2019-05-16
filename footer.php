25370bab29781979259c1e3e490c6aada896e165
Chetan Prajapati <hello@chetanprajapati.com>
2019-05-16 05:40:02 +0530

    Moved CTA to Footer.

--- footer.php
+++ footer.php
@@ -5,7 +5,10 @@
  * @package WPBBP
  */
 ?>
-
+	<section class="submit-idea-cta">
+		<h3><?php _e( 'Have an Idea for a Lesson or Workshop? Let us know!' ); ?></h3>
+		<a class="button button-primary" href="#"><?php _e( 'Submit an Idea' ); ?></a>
+	</section>
 	</div><!-- #content -->
 </div><!-- #page -->
 
