67d614fe157321dbc4e37cfd1984ed303f747ad2
Jesse C. Owens <jesse@jessecowens.com>
2019-08-05 19:05:33 -0600

    Match archive template to page-lesson-plans.php

--- themes/learn-theme-beta/archive.php
+++ themes/learn-theme-beta/archive.php
@@ -12,22 +12,12 @@ namespace WordPressdotorg\Theme;
 get_header();
 ?>
 
-	<main id="main" class="site-main col-8" role="main">
-
-	<?php if ( have_posts() ) : ?>
-
-		<header class="page-header">
-			<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="taxonomy-description">', '</div>' );
-			?>
-		</header><!-- .page-header -->
-
-		<?php
-		/* Start the Loop */
+<main id="main" class="site-main page-full-width" role="main">
+	<div id="lesson-plans" class="lp-list">
+<?php
+	if ( have_posts() ) :
 		while ( have_posts() ) :
 			the_post();
-
 			get_template_part( 'template-parts/content', 'archive' );
 		endwhile;
 
