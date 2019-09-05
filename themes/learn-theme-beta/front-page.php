e19e69f0e93b38b396a0649e4e690d0fbb17d456
juhi123 <juhi.saxena@rtcamp.com>
2019-09-05 18:31:18 +0530

    Convert Submit CTA in function and call it in required template

--- themes/learn-theme-beta/front-page.php
+++ themes/learn-theme-beta/front-page.php
@@ -92,11 +92,8 @@ get_header(); ?>
 			</div>
 		</section>
 
-		<section class="submit-idea-cta">
-			<h3><?php _e( 'Have an Idea for a Lesson or Workshop? Let us know!' ); ?></h3>
-			<a class="button button-primary" href="#"><?php _e( 'Submit an Idea' ); ?></a>
-		</section>
-
+		<?php wporg_submit_idea_cta(); ?>
+		
 		<?php if ( ! is_active_sidebar( 'front-page-blocks' ) ) : ?>
 			<?php //get_template_part( 'template-parts/bbpress', 'front' ); ?>
 		<?php else : ?>
