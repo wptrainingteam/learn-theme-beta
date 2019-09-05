7f247c86dba79854d03d8701dcf5794bb9aa8796
Chetan Prajapati <hello@chetanprajapati.com>
2019-09-05 16:48:55 +0530

    Single Lesson Plan template

--- themes/learn-theme-beta/template-parts/content-single.php
+++ themes/learn-theme-beta/template-parts/content-single.php
@@ -10,42 +10,46 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if ( is_page_template( 'page-forums-sidebar.php' ) ) : ?>
-		<?php bbp_breadcrumb(); ?>
-	<?php endif; ?>
-
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<section>
-			<div class="container">
-				<?php
-				the_content();
-
-				wp_link_pages(
-					array(
-						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wporg-forums' ),
-						'after'  => '</div>',
-					)
-				);
-				?>
+	<div class="entry-content">	
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</header><!-- .entry-header -->
+		<div class="lp-content">
+			<?php
+			the_content();
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wporg-forums' ),
+					'after'  => '</div>',
+				)
+			);
+			?>
+			<div class="lp-details">
+				<div class="left-items items">
+					<ul>
+						<li>
+							<span class="dashicons dashicons-clock"></span>
+							Length: <strong>1 Hour</strong>
+						</li>
+						<li>
+							<span class="dashicons dashicons-admin-users"></span>
+							Audience: <strong>Developers</strong>
+						</li>
+						<li>
+							<span class="dashicons dashicons-dashboard"></span>
+							Level: <strong>Beginning</strong>
+						</li>
+					</ul>
+				</div>
+				<div class="right-items items">
+					<ul>
+						<li>
+							<span class="dashicons dashicons-welcome-learn-more"></span>
+							Type of Instruction: <strong>Presentation, Demostration</strong>
+						</li>
+					</ul>
+				</div>
 			</div>
-		</section>
+		</div>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php
-		edit_post_link(
-			sprintf(
-				/* translators: %s: Name of current post */
-				esc_html__( 'Edit %s', 'wporg-forums' ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			),
-			'<span class="edit-link">',
-			'</span>'
-		);
-		?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
