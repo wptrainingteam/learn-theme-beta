24d7c242a79f9d8eae1ccfc69374a62c5a983acc
dufresnesteven <steve.dufresne@automattic.com>
2019-12-18 15:21:28 +0900

    Add breadrumb to the submit an idea page.

--- themes/learn-theme-beta/page-submit-an-idea.php
+++ themes/learn-theme-beta/page-submit-an-idea.php
@@ -8,47 +8,36 @@
 get_header(); ?>
 
 	<main id="main" class="site-main page-full-width" role="main">
+		<?php echo get_template_part( 'template-parts/component', 'breadcrumbs' );  ?>
 
 		<?php
 		while ( have_posts() ) :
 			the_post();
 			?>
-				<div class="clearfix">
-					<div class="bbp-breadcrumb">
-						<p>
-							<a href="<?php echo home_url(); ?>" class="bbp-breadcrumb-home">Learn Home</a>
-							<span class="bbp-breadcrumb-sep">»</span>
-							<span class="bbp-breadcrumb-current">Submit an Idea</span>
-						</p>
-					</div>
-				</div>
+			<div id="submit-an-idea" class="type-page">
+				<div class="entry-content">
+					<section class="submit-idea-cta">
+						<h4><?php _e( 'Is this a Workshop or Lesson Plan idea?' ); ?></h4>
 
-				<div id="submit-an-idea" class="type-page">
-					<div class="entry-content">
-						<section class="submit-idea-cta">
-							<h4><?php _e( 'Is this a Workshop or Lesson Plan idea?' ); ?></h4>
-
-							<div class="idea-type-lists">
-								<div class="col">
-									<span class="dashicons dashicons-welcome-learn-more"></span>
-									<p>
-									Workshops are a collection of leesions and a great way to get people hands-on with WordPress.
-									</p>
-									<a class="button button-primary button-large" href="/submit-workshop-idea"><?php _e( 'Workshop Idea' ); ?></a>
-								</div>
-								
-								<div class="col">
-									<span class="dashicons dashicons-lightbulb"></span>
-									<p>
-									Lesson plans are designed to guide and inspire others to deliver great content.
-									</p>
-									<a class="button button-primary button-large" href="/submit-lesson-idea"><?php _e( 'Lesson Idea' ); ?></a>
-								</div>
+						<div class="idea-type-lists">
+							<div class="col">
+								<span class="dashicons dashicons-welcome-learn-more"></span>
+								<p>
+								Workshops are a collection of leesions and a great way to get people hands-on with WordPress.
+								</p>
+								<a class="button button-primary button-large" href="/submit-workshop-idea"><?php _e( 'Workshop Idea' ); ?></a>
 							</div>
-
-						</section>
+							
+							<div class="col">
+								<span class="dashicons dashicons-lightbulb"></span>
+								<p>
+								Lesson plans are designed to guide and inspire others to deliver great content.
+								</p>
+								<a class="button button-primary button-large" href="/submit-lesson-idea"><?php _e( 'Lesson Idea' ); ?></a>
+							</div>
+						</div>
+					</section>
 				</div>
-
 			</div>
 			<?php
 		endwhile; // End of the loop.
